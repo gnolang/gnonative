7122ecd49d89da90a99b1d85156fc06ff792d0ff
D4ryl00 <d4ryl00@gmail.com>
2023-08-11 09:08:04 +0200

    feat: setup ios project
    Signed-off-by: D4ryl00 <d4ryl00@gmail.com>

--- framework/gnokey.go
+++ framework/gnokey.go
@@ -2,6 +2,11 @@ package gnomobile
 
 import "fmt"
 
+type PromiseBlock interface {
+	CallResolve(reply string)
+	CallReject(error error)
+}
+
 func Hello(name string) string {
 	return fmt.Sprintf("Hello, %s", name)
 }
---
+++ gnoboard/ios/gnoboard/Sources/Bridge/GoBridge.m
@@ -0,0 +1,20 @@
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(GoBridge, NSObject)
+
+RCT_EXTERN_METHOD(initBridge:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject);
+
+RCT_EXTERN_METHOD(closeBridge:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject);
+
+RCT_EXTERN_METHOD(hello:(NSString)name
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject);
+
++ (BOOL)requiresMainQueueSetup
+{
+ return YES;  // only do this if your module initialization relies on calling UIKit!
+}
+
+@end
