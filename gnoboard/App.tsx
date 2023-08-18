2cde5d2d4389bb2cdafa737716622dd21a651b25
Iuri Pereira <iuricmp@gmail.com>
2023-08-18 16:44:34 +0100

    feat: initial user interface
    Adds initial screens to manage the GNO integration.
    
    Signed-off-by: Iuri Pereira <iuricmp@gmail.com>

--- gnoboard/App.tsx
+++ gnoboard/App.tsx
@@ -1,32 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
-import { GoBridge } from '@gno/native_modules/GoBridge';
+import { GoBridge } from "@gno/native_modules/GoBridge";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [greeting, setGreeting] = React.useState<string>('');
+  const [greeting, setGreeting] = React.useState<string>("");
 
   useEffect(() => {
-    const getGreeting = async () => setGreeting(await GoBridge.hello('Gno.land'));
+    const getGreeting = async () =>
+      setGreeting(await GoBridge.hello("Gno.land"));
 
     getGreeting();
-    }, []);
-
+  }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>{greeting}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
