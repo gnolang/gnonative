d92468416255ab523fc6ed176b218ce50fd4fe44
Iuri Pereira <iuricmp@gmail.com>
2023-10-09 15:42:07 +0100

    chore: replace native bridge

--- gnoboard/App.tsx
+++ gnoboard/App.tsx
@@ -1,24 +1,6 @@
-import { useGno } from '@gno/hooks/use-gno';
-import Loading from '@gno/screens/loading';
 import CustomRouter from '@gno/router/custom-router';
-import { useEffect, useState } from 'react';
 import 'fast-text-encoding';
 
 export default function App() {
-  const gno = useGno();
-  const [loading, setLoading] = useState<string | undefined>(undefined);
-
-  useEffect(() => {
-    setLoading('Initializing bridge...');
-    gno
-      .initBridge()
-      .catch((error) => console.log(error))
-      .finally(() => setLoading(undefined));
-  }, []);
-
-  if (loading) {
-    return <Loading message={loading} />;
-  }
-
   return <CustomRouter />;
 }
--- gnoboard/src/grpc/client.ts
+++ gnoboard/src/grpc/client.ts
@@ -1,13 +1,14 @@
-import { Code, ConnectError, createPromiseClient } from "@connectrpc/connect";
-import { createXHRGrpcWebTransport } from "./transport";
-import { GnomobileService } from "@gno/api/rpc_connect";
+import { createPromiseClient } from '@connectrpc/connect';
+import { createXHRGrpcWebTransport } from './transport';
+import { GnomobileService } from '@gno/api/rpc_connect';
 
 // Create a GnomobileService client
-export function createClient(port: Number) {
+export function createClient(port: number) {
+  console.log('createClient', port)
   return createPromiseClient(
     GnomobileService,
     createXHRGrpcWebTransport({
-      baseUrl: "http://127.0.0.1:" + port,
+      baseUrl: 'http://127.0.0.1:' + port,
     }),
   );
 }
