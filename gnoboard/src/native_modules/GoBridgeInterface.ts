261ac3b3584aa297840b09ad43397de395c9e483
Iuri <iuricmp@gmail.com>
2023-11-03 11:10:42 +0000

    feat: Check if Account Name exists (#63)
    This PR implements a React Native
    [Modal](https://reactnative.dev/docs/modal) to confirm if the user wants
    to override an existing account.
    
    <img width="200"
    
    src="https://github.com/gnolang/gnomobile/assets/689440/b8dc53dd-df6a-4155-bd50-5e6d367eb216"
    />
    
    Fix for #30

--- gnoboard/src/native_modules/GoBridgeInterface.ts
+++ gnoboard/src/native_modules/GoBridgeInterface.ts
@@ -1,5 +1,3 @@
-import { GnoAccount } from "./types";
-
 export interface GoBridgeInterface {
   initBridge(): Promise<void>;
   closeBridge(): Promise<void>;
--- gnoboard/src/screens/certify/create-password/index.tsx
+++ gnoboard/src/screens/certify/create-password/index.tsx
@@ -10,6 +10,8 @@ import { useNavigation } from '@react-navigation/native';
 import { RoutePath } from '@gno/router/path';
 import SeedBox from '@gno/components/seedbox';
 import Alert from '@gno/components/alert';
+import { Spacer } from '@gno/components/row';
+import { ModalConfirm } from '@gno/components/modal';
 
 const text = {
   title: 'Create\na Password',
@@ -24,11 +26,12 @@ const CreatePassword: React.FC<Props> = ({ route }) => {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState<string | undefined>(undefined);
+  const [showModal, setShowModal] = useState(false);
 
   const gno = useGno();
   const navigation = useNavigation<RouterWelcomeStackProp>();
 
-  const onSaveHandler = async () => {
+  const createAccount = async (override: boolean = false) => {
     if (!name || !password || !confirmPassword) return;
 
     if (password !== confirmPassword) {
@@ -38,36 +41,55 @@ const CreatePassword: React.FC<Props> = ({ route }) => {
     }
 
     try {
+      if (!override) {
+        const hasKeyByName = await gno.hasKeyByName(name);
+        if (hasKeyByName) {
+          setShowModal(true);
+          return;
+        }
+      }
+
       const response = await gno.createAccount(name, phrase, password);
       console.log('createAccount response: ' + response);
       await gno.selectAccount(name);
       await gno.setPassword(password);
       navigation.navigate(RoutePath.Home);
     } catch (error) {
-      console.log(error);
+      setError(JSON.stringify(error));
+      console.log(JSON.stringify(error));
     }
   };
 
   return (
-    <Layout.Container>
-      <Layout.Header />
-      <Layout.Body>
-        <Text.Title>{text.title}</Text.Title>
-        <Text.Body>{text.desc}</Text.Body>
-        <Alert severity='error' message={error} />
-        <TextInput placeholder='Account Name' value={name} onChangeText={setName} />
-        <TextInput placeholder='Password' value={password} onChangeText={setPassword} secureTextEntry={true} error={error} />
-        <TextInput
-          placeholder='Confirm Password'
-          value={confirmPassword}
-          onChangeText={setConfirmPassword}
-          secureTextEntry={true}
-          error={error}
-        />
-        <SeedBox placeholder='Your seed phrase' value={phrase} editable={false} />
-        <Button title='Save' onPress={onSaveHandler} variant='primary' />
-      </Layout.Body>
-    </Layout.Container>
+    <>
+      <Layout.Container>
+        <Layout.Header />
+        <Layout.Body>
+          <Text.Title>{text.title}</Text.Title>
+          <Text.Body>{text.desc}</Text.Body>
+          <Alert severity='error' message={error} />
+          <TextInput placeholder='Account Name' value={name} onChangeText={setName} autoCapitalize='none' />
+          <TextInput placeholder='Password' value={password} onChangeText={setPassword} secureTextEntry={true} error={error} />
+          <TextInput
+            placeholder='Confirm Password'
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            secureTextEntry={true}
+            error={error}
+          />
+          <SeedBox placeholder='Your seed phrase' value={phrase} editable={false} />
+          <Spacer />
+          <Button title='Save' onPress={() => createAccount()} variant='primary' />
+        </Layout.Body>
+      </Layout.Container>
+      <ModalConfirm
+        title={`Overriding ${name}'s Account`}
+        message='This account name is already in use. Are you sure you want to override the existing account?'
+        visible={showModal}
+        onClose={() => setShowModal(false)}
+        onConfirm={() => createAccount(true)}
+      />
+    </>
   );
 };
 
