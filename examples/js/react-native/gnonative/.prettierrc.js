d44921284310b4c206943c0669ff63f3c16330f1
D4ryl00 <d4ryl00@gmail.com>
2024-03-18 16:39:15 +0100

    feat: add prettier rules
    Signed-off-by: D4ryl00 <d4ryl00@gmail.com>

---
+++ examples/js/react-native/gnonative/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'all',
+  semi: true,
+  arrowParens: 'avoid',
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+}
